cca1456e199421b6dabfa88957fe8e6a3045378f
milindasandaru <sams.senarath@gmail.com>
2026-01-21 22:41:53 +0530

    implementing header.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,14 @@
 import { ReactNode } from 'react';
+import Header from '@/components/Header';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <main className="root-container">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">Header</div>
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">{children}</div>
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <Header />
+
+        <div className="mt-20 pb-20">{children}</div>
+      </div>
     </main>
   );
 };
